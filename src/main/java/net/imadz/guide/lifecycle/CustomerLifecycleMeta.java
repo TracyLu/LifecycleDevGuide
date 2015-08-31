7567c8c07c7d9bfa6b946f2e59cb3e7cfa9fa37d
Tracy Lu <hailinglu@gmail.com>
2015-08-31 16:33:44 +0800

    simple-fsm add initial state

--- src/main/java/net/imadz/guide/lifecycle/CustomerLifecycleMeta.java
+++ src/main/java/net/imadz/guide/lifecycle/CustomerLifecycleMeta.java
@@ -3,6 +3,7 @@ package net.imadz.guide.lifecycle;
 import net.imadz.lifecycle.annotations.EventSet;
 import net.imadz.lifecycle.annotations.StateMachine;
 import net.imadz.lifecycle.annotations.StateSet;
+import net.imadz.lifecycle.annotations.state.Initial;
 
 /**
  * Created by Tracy on 8/25/15.
@@ -11,7 +12,8 @@ import net.imadz.lifecycle.annotations.StateSet;
 public interface CustomerLifecycleMeta {
     @StateSet
     static interface States {
-
+        @Initial
+        static interface Draft {}
     }
 
 }
