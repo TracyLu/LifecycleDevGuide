a627fb5757a0d142ab55a5f82980e60316e4c0aa
Tracy Lu <hailinglu@gmail.com>
2015-08-31 20:35:42 +0800

    simple fsm specify fsm metadata to Customer class

--- src/main/java/net/imadz/guide/impl/Customer.java
+++ src/main/java/net/imadz/guide/impl/Customer.java
@@ -1,9 +1,12 @@
 package net.imadz.guide.impl;
 
+import net.imadz.guide.lifecycle.CustomerLifecycleMeta;
+import net.imadz.lifecycle.annotations.LifecycleMeta;
+
 /**
  * Created by Tracy on 8/25/15.
  */
-
+@LifecycleMeta(value = CustomerLifecycleMeta.class)
 public class Customer {
 
 }
