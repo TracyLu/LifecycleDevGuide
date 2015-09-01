de20851be40b2334c29fd727ce7b2150c172364a
Tracy Lu <hailinglu@gmail.com>
2015-09-01 12:05:31 +0800

    simple fsm add state indicator to Customer

--- src/main/java/net/imadz/guide/impl/Customer.java
+++ src/main/java/net/imadz/guide/impl/Customer.java
@@ -2,11 +2,17 @@ package net.imadz.guide.impl;
 
 import net.imadz.guide.lifecycle.CustomerLifecycleMeta;
 import net.imadz.lifecycle.annotations.LifecycleMeta;
+import net.imadz.lifecycle.annotations.StateIndicator;
 
 /**
  * Created by Tracy on 8/25/15.
  */
 @LifecycleMeta(value = CustomerLifecycleMeta.class)
 public class Customer {
+    @StateIndicator
+    private String state = null;
 
+    public String getState() {
+        return state;
+    }
 }
