8e41fa55234261f59ac3beece29509585a27a60a
Tracy Lu <hailinglu@gmail.com>
2015-09-01 11:57:20 +0800

    Revert "simple fsm init state of Customer"
    This reverts commit b5992406382e14b636aef12ac73a709f8024315b.

--- src/main/java/net/imadz/guide/impl/Customer.java
+++ src/main/java/net/imadz/guide/impl/Customer.java
@@ -9,9 +9,6 @@ import net.imadz.lifecycle.annotations.StateIndicator;
  */
 @LifecycleMeta(value = CustomerLifecycleMeta.class)
 public class Customer {
-    public Customer() {
-        state = CustomerLifecycleMeta.States.Draft.class.getSimpleName();
-    }
     @StateIndicator
     private String state = null;
 }
